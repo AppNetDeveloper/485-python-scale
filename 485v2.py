60e5eeab314201b12a26f7fa1a3cc1018467c0c2
Liviu diaconu <liviudiaconu@appnet.dev>
2025-01-20 15:40:55 +0100

    Update 485v2.py

--- 485v2.py
+++ 485v2.py
@@ -1,12 +1,8 @@
 import time
-import json
 from pymodbus.client import ModbusSerialClient
 import paho.mqtt.client as mqtt
+import json
 import threading
-
-# Cargar la configuración desde el archivo JSON
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
 
 # Crear un bloqueo para evitar conflictos en el uso del cliente Modbus y un evento para pausar/continuar la lectura de peso
 modbus_lock = threading.Lock()
@@ -14,29 +10,22 @@ pause_event = threading.Event()
 
 # Configuración del cliente Modbus
 client = ModbusSerialClient(
-    port=config['modbus']['port'],
-    baudrate=config['modbus']['baudrate'],
-    timeout=config['modbus']['timeout'],
-    stopbits=config['modbus']['stopbits'],
-    bytesize=config['modbus']['bytesize'],
-    parity=config['modbus']['parity']
+    port='/dev/ttyUSB0',  # Cambia al puerto adecuado en Linux
+    baudrate=9600,
+    timeout=1,
+    stopbits=1,
+    bytesize=8,
+    parity='N'
 )
 
 # Configuración del cliente MQTT
-mqtt_broker = config["mqtt_broker"]
-mqtt_base_topic = config["mqtt_base_topic"]
-mqtt_status_topic = config["mqtt_status_topic"]
-mqtt_dosificador_topic = config["mqtt_dosificador_topic"]
-mqtt_zero_topic = config["mqtt_zero_topic"]
-mqtt_tara_topic = config["mqtt_tara_topic"]
-mqtt_cancel_topic = config["mqtt_cancel_topic"]
-
-# Ignorar advertencias de deprecación para la API de callbacks de MQTT
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-# Crear el cliente MQTT con la versión más reciente
-mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_broker = "localhost"  # IP de tu broker MQTT
+mqtt_base_topic = "sensorica/bascula/peso"
+mqtt_status_topic = "sensorica/bascula/status"
+mqtt_dosificador_topic = "sensorica/bascula/dosifica"
+mqtt_zero_topic = "sensorica/bascula/zero"
+mqtt_tara_topic = "sensorica/bascula/tara"
+mqtt_cancel_topic = "sensorica/bascula/cancel"
 
 def on_connect(mqtt_client, userdata, flags, rc):
     if rc == 0:
@@ -50,9 +39,6 @@ def on_connect(mqtt_client, userdata, flags, rc):
 
 def on_message(mqtt_client, userdata, message):
     topic = message.topic
-    if "/status" in topic:  # Ignorar mensajes de estado
-        return
-
     try:
         # Verificar si el payload no está vacío y es decodificable
         if message.payload:
@@ -70,7 +56,7 @@ def on_message(mqtt_client, userdata, message):
                 pause_event.set()  
                 return
 
-            if config['modbus_address_range']['start'] <= direccion_modbus <= config['modbus_address_range']['end']:
+            if 2 <= direccion_modbus <= 8:
                 if mqtt_dosificador_topic in topic and 'value' in payload:
                     iniciar_dosificacion(direccion_modbus, payload['value'])
                 elif mqtt_zero_topic in topic and 'value' in payload and payload['value'] is True:
@@ -87,7 +73,7 @@ def on_message(mqtt_client, userdata, message):
                         mqtt_client.publish(mqtt_topic, payload_tara)
                         print(f"Publicado en MQTT - Tópico: {mqtt_topic}, Payload: {payload_tara}")
             else:
-                print(f"Error: Dirección Modbus fuera del rango permitido ({config['modbus_address_range']['start']}-{config['modbus_address_range']['end']}). Dirección recibida: {direccion_modbus}")
+                print(f"Error: Dirección Modbus fuera del rango permitido (2-8). Dirección recibida: {direccion_modbus}")
 
             pause_event.set()
 
@@ -99,14 +85,12 @@ def on_message(mqtt_client, userdata, message):
 
 def publicar_estado_operacion(direccion_modbus, operacion, estado):
     """ Publica el estado de la operación en el tópico MQTT correspondiente """
-    mqtt_topic = f"sensorica/bascula/{operacion}/{direccion_modbus}/status"
+    mqtt_topic = f"sensorica/bascula/{operacion}/status"
     payload = json.dumps({"status": estado})
     mqtt_client.publish(mqtt_topic, payload)
     print(f"Publicado en MQTT - Tópico: {mqtt_topic}, Payload: {payload}")
 
 def iniciar_dosificacion(direccion_modbus, peso_objetivo):
-    # Primero, cancelar cualquier dosificación en curso
-    cancelar_dosificacion(direccion_modbus)
     
     publicar_estado_operacion(direccion_modbus, "dosifica", "Iniciando")
     with modbus_lock:
@@ -156,24 +140,10 @@ def hacer_tara(direccion_modbus):
             print(f"Error al realizar la tara: {e}")
             publicar_estado_operacion(direccion_modbus, "tara", "ERROR")
 
-def leer_valor_tara(direccion_modbus):
-    with modbus_lock:
-        try:
-            response = client.read_holding_registers(1002, 2, slave=direccion_modbus)
-            if not response.isError():
-                tara_value = (response.registers[0] << 16) + response.registers[1]
-                return tara_value / 10.0
-            else:
-                print(f"Error al leer la tara en dirección Modbus {direccion_modbus}")
-                return None
-        except Exception as e:
-            print(f"Error al leer la tara: {e}")
-            return None
-
 def reconectar_modbus():
     while not client.connect():
-        print("Error al conectar con el puerto Modbus. Reintentando en", config['reconnect_interval'], "segundos...")
-        time.sleep(config['reconnect_interval'])
+        print("Error al conectar con el puerto Modbus. Reintentando en 5 segundos...")
+        time.sleep(5)
     print("Conexión Modbus restablecida.")
 
 def reconectar_mqtt():
@@ -184,8 +154,8 @@ def reconectar_mqtt():
             print("Conexión MQTT restablecida.")
             break
         except Exception as e:
-            print(f"Error al conectar con el broker MQTT: {e}. Reintentando en {config['reconnect_interval']} segundos...")
-            time.sleep(config['reconnect_interval'])
+            print(f"Error al conectar con el broker MQTT: {e}. Reintentando en 5 segundos...")
+            time.sleep(5)
 
 def enviar_estado():
     while True:
@@ -210,36 +180,36 @@ def escanear_y_leer_peso():
         with modbus_lock:
             try:
                 registro_peso_base = 9
-                for direccion in range(config['modbus_address_range']['start'], config['modbus_address_range']['end'] + 1):
+                for direccion in range(2, 8):
                     try:
                         response = client.read_input_registers(registro_peso_base, 2, slave=direccion)
-                        if not response.isError():
+                        if response.isError():
+                            print(f"Sin respuesta en la dirección: {direccion}")
+                        else:
                             peso_neto = (response.registers[0] << 16) + response.registers[1]
                             peso_neto /= 10.0
 
                             # Validar si el valor es mayor al límite permitido
                             if peso_neto > 999999:
                                 peso_neto = 0  # Ajustar a 0 si el valor es inválido
-                                print(f"Valor inválido recibido: {peso_neto}. Se ajustará a 0 y se hará un cero.")
-                                hacer_cero(direccion)  # Realizar la operación de cero
+                                print(f"Valor inválido recibido: {peso_neto}. Se ajustará a 0.")
 
                             mqtt_topic = f"{mqtt_base_topic}/{direccion}"
                             payload = json.dumps({"value": peso_neto})
                             mqtt_client.publish(mqtt_topic, payload)
-                            print(f"Peso publicado en MQTT - Tópico: {mqtt_topic}, Payload: {payload}")
-                        else:
-                            print(f"Sin respuesta en la dirección: {direccion}")
                     except Exception as e:
                         print(f"Error al escanear la dirección {direccion}: {e}")
-                time.sleep(config['scan_interval'])
+                time.sleep(0.1)
             except Exception as e:
-                print(f"Error general en el proceso: {e}. Reintentando en {config['reconnect_interval']} segundos...")
+                print(f"Error general en el proceso: {e}. Reintentando en 5 segundos...")
                 reconectar_modbus()
 
 if __name__ == "__main__":
+    mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
 
+    # Iniciar la reconexión MQTT y el hilo de estado
     reconectar_mqtt()
     threading.Thread(target=enviar_estado, daemon=True).start()
     pause_event.set()
